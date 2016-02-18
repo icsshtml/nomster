a9d8e9309f040abe94e9575e4bd5ee8bcef7aea3
Jonathan Li <icsshtml@gmail.com>
2016-02-18 22:22:56 +0000

    completed lesson 24

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -25,8 +25,16 @@ class PlacesController < ApplicationController
 	def update
 		@place = Place.find(params[:id])
 		@place.update_attributes(place_params)
+
 		redirect_to root_path
 	end
+
+	def destory
+		@place = Place.find(params[:id])
+		@place.destory
+		redirect_to root_path
+	end 
+
 
 	private
 
