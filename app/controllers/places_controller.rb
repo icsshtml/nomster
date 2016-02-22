316c28c1fd34ab63f10531466b67e9120311b1ba
Jonathan Li <icsshtml@gmail.com>
2016-02-22 18:13:35 +0000

    completed lesson 26

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+	
 	def index
 		@places = Place.all
 		@places = Place.paginate(:page => params[:page], :per_page => 1)
@@ -20,18 +21,25 @@ class PlacesController < ApplicationController
 
 	def edit
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render text: 'Not Allowed', status: :forbidden
+		end
 	end
 
 	def update
 		@place = Place.find(params[:id])
-		@place.update_attributes(place_params)
 
+		if @place.user != current_user
+			return render text: 'Not Allowed', status: :forbidden
+		end
+		
+		@place.update_attributes(place_params)
 		redirect_to root_path
 	end
 
-	def destoy
+	def destroy
 		@place = Place.find(params[:id])
-		@place.destory
+		@place.destroy
 		redirect_to root_path
 	end 
 
