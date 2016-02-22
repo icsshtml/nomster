55fb7d4ca16be11447a5b08046e414ad22925cc4
Jonathan Li <icsshtml@gmail.com>
2016-02-22 19:01:58 +0000

    simple_form design

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,8 +11,12 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :new, status: :unprocessable_entity
+		end
 	end
 
 	def show
@@ -30,11 +34,15 @@ class PlacesController < ApplicationController
 		@place = Place.find(params[:id])
 
 		if @place.user != current_user
-			return render text: 'Not Allowed', status: :forbidden
+			return render text: 'Not Allowed testing', status: :forbidden
 		end
 
 		@place.update_attributes(place_params)
-		redirect_to root_path
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :edit, status: :unprocessable_entity
+		end
 	end
 
 	def destroy
@@ -42,7 +50,7 @@ class PlacesController < ApplicationController
 		if @place.user != current_user
 			return render text: 'Not Allowed', status: :forbidden
 		end
-		
+
 		@place.destroy
 		redirect_to root_path
 	end 
