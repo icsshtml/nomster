062ecfce12e7ea5d236f585c3cef155462434e01
Jonathan Li <icsshtml@gmail.com>
2016-02-27 00:28:08 +0000

    completed lesson38

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,11 +1,15 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
-	
+
 	geocoded_by :address
   	after_validation :geocode
 
 	validates :name, presence: true, length: { minimum: 3 }
 	validates :address, presence: true
 	validates :description, presence: true
+
+	def last_comment
+		self.comments.order("id ASC").last
+	end
 end
